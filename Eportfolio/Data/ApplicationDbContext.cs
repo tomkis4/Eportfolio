4d7f391ffc4f56466d47d718944a6a392d29d57f
tomkis4 <129515242+tomkis4@users.noreply.github.com>
2024-04-30 15:52:56 +0200

    Pierwszy commit

---
+++ Eportfolio/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Eportfolio.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eportfolio.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ Eportfolio/Models/Posts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Eportfolio.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Tytuł jest wymagany.")]
+        [StringLength(100, ErrorMessage = "Tytuł nie może być dłuższy niż 100 znaków.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Treść jest wymagana.")]
+        public string Content { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; }
+    }
+}
